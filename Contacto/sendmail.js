cddca1e23b5f467a8d9912bf136bc17b7d4e95a5
Emma Gaxiola <emma.gaxiol@gmail.com>
2024-06-30 14:15:42 -0600

    add: formulario, validation, send email.

---
+++ Contacto/sendmail.js
@@ -0,0 +1,10 @@
+function sendEmail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+        phone : document.getElementById("phone").value,
+
+    }
+    emailjs.send("service_nao4sns","template_hc4na4l", parms).then(alert("Message Sent Succesfully"));
+}
