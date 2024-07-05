0189fe488a14e3e20f16b655f92b6041e3d35385
Emma Gaxiola <emma.gaxiol@gmail.com>
2024-07-05 16:53:10 -0600

    edit: style

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import { navmenuApp } from './src/components/navmenu/navmenu-app';
+import { navmenuApp } from './src/components/navmenu/navmenu-app.js';
 
 document.getElementById('navmenu-app').innerHTML =  navmenuApp();
