ebbc4389d2c975f8af2d1658823ec92a465020f6
Emma Gaxiola <emma.gaxiol@gmail.com>
2024-07-10 18:13:10 -0600

    edit: fix filepaths

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import { navmenuApp } from '/src/components/navmenu/navmenu-app.js';
+import { navmenuApp } from './src/components/navmenu/navmenu-app.js';
 
 document.querySelector("#navmenu-app").innerHTML =  navmenuApp();
