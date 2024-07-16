43977b04cdb8dbc1d53a7ee70b23e9d9e739dd7e
Emma Gaxiola <emma.gaxiol@gmail.com>
2024-07-15 21:55:31 -0600

    add: loader.js

--- index.js
+++ index.js
@@ -2,17 +2,7 @@ import { navmenuApp } from './src/components/navmenu/navmenu-app.js';
 
 document.querySelector("#navmenu-app").innerHTML =  navmenuApp();
 
-const loader = document.querySelector(".preloader");
 
-window.addEventListener("load", function(){
-    setTimeout(() => {
-        loader.style.opacity = "0";
-    }, 1000);
-    setTimeout(() => {
-        loader.style.display = "none";
-    }, 1300);
-    
-})
 
 function saveData(){    
     let inputName = document.getElementById("name").value;
